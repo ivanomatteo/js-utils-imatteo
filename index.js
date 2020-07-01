0074c17d8cdff36c0c7b59bd79955ca10a523171
Ivano Matteo <ivanomatteo@gmail.com>
2020-07-01 10:38:17 +0200

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+exports.escapeRegExp = function (string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+
+exports.objToQuery = function(obj) {
+    let encVal = (v) => {
+        if (typeof v === 'object') {
+            v = JSON.stringify(v);
+        }
+        return encodeURIComponent(v);
+    };
+    let res = [];
+    for (let k in obj) {
+        if (Array.isArray(obj[k])) {
+            res = res.concat(obj[k].map(el => encVal(k) + '[]=' + encVal(el)));
+        } else {
+            res.push(encVal(k) + '=' + encVal(obj[k]));
+        }
+    }
+    return res.join('&');
+}
+
+
